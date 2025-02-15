509e60c778c2a69edffc34ac1ebd4451913ebe6c
PRITAM SAHA <pritamstech@hotmail.com>
2025-02-16 00:36:34 +0530

    Commited One Temporary File

---
+++ NPTEL/Temp.java
@@ -0,0 +1,22 @@
+class Person {
+    int a=1;
+    int b=0;
+    public Person(){
+        System.out.println(a+b+" Java ");
+    }
+}
+class Employee extends Person {
+    int a=0;
+    int b=1;
+    public Employee(){
+        System.out.println(a*b+" Java ");
+    }
+}
+
+public class Temp {
+    public static void main(String[] args) {
+        Person p=new Employee();
+    
+    }
+
+}
